f445a801e18d65801e0eaf25d10ce15dd5badb19
Yashvi Desai <54462747+YashviDesaii@users.noreply.github.com>
2023-12-18 19:56:52 -0800

    PC-1 Ex1 Done

--- Exercise_1.java
+++ Exercise_1.java
@@ -8,29 +8,60 @@ class Stack {
     boolean isEmpty() 
     { 
         //Write your code here 
+    	if (top==-1){
+ 
+    		return true;
+    	}
+    	return false;
+    } 
+    boolean isFull() 
+    { 
+        //Write your code here 
+    	if (top==MAX-1){
+ 
+    		return true;
+    	}
+    	return false;
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+    	top=-1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+    	if (isFull()) {
+    		System.out.println("Stack Overflow");
+    	}
+    	top+=1;
+    	a[top]=x;
+    	//a[++top]=x
+    	return true;
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
+    	if (isEmpty()) {
+    		System.out.print("Stack underflow");;
+    	}
+    	
         //Write your code here
+    	
+    	return a[top--];
     } 
   
     int peek() 
     { 
         //Write your code here
+    	return a[top];
     } 
+    
+    
 } 
   
 // Driver code 
